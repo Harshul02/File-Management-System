0eed716bc8870d07984ae3c56dc09c036d671e6b
Harshul Jain <harshulgangwal02@gmail.com>
2024-01-16 13:45:46 +0530

    Added UI to home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://img.freepik.com/free-vector/yellow-house_1110-691.jpg?w=740&t=st=1700549866~exp=1700550466~hmac=49ba49076a52603bc22a57463b23bf0eed2208a2aa7384bf04ebb09474dd5854" className="h-10" alt="Flowbite Logo" />
+            <img src="https://play-lh.googleusercontent.com/2oyw6who4WdYH2INRG1qoYS3nvC0hWOHlGC50GEADfhnXonDrCLKg_eEVT5fWqAK4ME" className="h-10" alt="Flowbite Logo" />
             <span className="mx-4 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">File Manager</span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from 'react';
+import NavBar from '../components/NavBar';
 
 const HomePage = () => {
   return (
     <div>
-      <NavBar />
-    </div>
-  )
-}
+      <div>
+        <NavBar />
+      </div>
+      <div className="mt-20 container mx-auto h-full">
+        <div className="grid grid-cols-2 gap-4 p-4 mt-4 h-full relative" style={{marginTop: "200px"}}>
+          {/* Left div with centered text */}
+          <div className="flex items-center justify-center ">
+            <h2 className="text-7xl font-medium italic mx-4">Welcome to File Manager</h2>
+          </div>
 
-export default HomePage
+          {/* Right div with a centered image */}
+          <div className="flex items-center justify-center ">
+            <img
+              src="https://play-lh.googleusercontent.com/2oyw6who4WdYH2INRG1qoYS3nvC0hWOHlGC50GEADfhnXonDrCLKg_eEVT5fWqAK4ME"
+              alt="Image Alt Text"
+            //   className="w-full h-full"
+              style={{ width: '40%', height: '100%' }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='absolute bottom-0 w-full mt-auto'>
+      <div className="text-xl py-8 bg-blue-300 text-center text-white h-full" style={{fontFamily: "'Salsa', cursive"}}>
+        <footer>
+          Made with <span style={{color: "red"}}>❤</span> by Harshul Jain
+        </footer>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
